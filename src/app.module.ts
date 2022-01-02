7edd1e781f61f21910a493ee114b455b46692a40
muonline.mega@gmail.com <muonline.mega@gmail.com>
2022-01-02 03:26:31 +0200

    work in progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { MovieController } from './Controllers/movie/movie.controller';
 import { MovieService } from './services/movie/movie.service';
 import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
     HttpModule,
